fd409861eb5f9b714ca7736783a19e72b239a355
Clement Prolifel Priyatama <clementprolifel@gmail.com>
2022-03-29 22:25:37 +0700

    change to php

--- index.php
+++ index.php
@@ -1,1 +1,66 @@
-<?php include_once("index.html"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Clement Prolifel Priyatama</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="My name is Clement Prolifel Priyatama. I come from Malang, Indonesia and still a computer science's student at Institut Teknologi Sepuluh Nopember">
+    <meta name="robots" content="index, follow" />
+
+    <meta property="og:title" content="Clement Prolifel Priyatama">
+    <meta property="og:description" content="My name is Clement Prolifel Priyatama. I come from Malang, Indonesia and still a computer science's student at Institut Teknologi Sepuluh Nopember">
+    <meta property="og:image" content="https://prolifel.com/icon.jpg">
+    <meta property="og:url" content="https://prolifel.com">
+
+    <meta name="twitter:title" content="Clement Prolifel Priyatama">
+    <meta name="twitter:description" content="My name is Clement Prolifel Priyatama. I come from Malang, Indonesia and still a computer science's student at Institut Teknologi Sepuluh Nopember'">
+    <meta name="twitter:url" content="https://prolifel.com/icon.jpg">
+    <meta name="twitter:card" content="summary">
+
+    <meta name="keywords" content="clement prolifel priyatama clementprolifel">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+    <title>prolifel.com</title>
+
+    <style>
+        h1 {
+            font-family: monospace;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Hello!</h1>
+    <pre style="word-wrap: break-word; white-space: pre-wrap">
+
+Wondering why my personal website looks like Tim Berners-Lee's first post?
+Yea, thank you Mr. Tim. You're our savior 🚀 (with extra modern detail lol)
+
+
+My name is Clement Prolifel Priyatama.
+I come from Malang, Indonesia and still a computer science's student at 
+Institut Teknologi Sepuluh Nopember
+(btw you can look my uni in <a href="https://www.its.ac.id/">here</a>)
+
+I do code with PHP and some knowledge in python and javascript.
+
+
+You can reach me on:
+
+1. Github for personal and professional projects, click <a href="https://github.com/prolifel">here</a>!
+2. Email <a href="mailto:clementprolifel@gmail.com">here!</a>
+3. Also, LinkedIn <a href="https://www.linkedin.com/in/prolifel/">here</a>
+
+I also have a blog on Medium too!
+<a href="https://prolifel.medium.com">prolifel.medium.com</a>
+
+
+Thank you!
+Terima kasih!
+    </pre>
+    <!-- </body> -->
+
+</html>
